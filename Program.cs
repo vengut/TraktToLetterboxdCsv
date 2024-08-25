33bffcecb2a568ebd0da5ebaad0ee26b1d5527b0
Venkata Gururamiah <gvenkat96@gmail.com>
2024-08-25 16:45:21 -0400

    Read From Trakt CSV

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+using TraktToLetterboxdCsv;
+
+// https://letterboxd.com/about/importing-data/
+
+using var reader = new StreamReader("../../../trakt-movies.csv");
+using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
+var records = csv.GetRecords<TraktWatch>();
+Console.WriteLine(records.GroupBy(x => x.ImdbId).Count());
---
+++ TraktWatch.cs
@@ -0,0 +1,15 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace TraktToLetterboxdCsv;
+
+internal record TraktWatch
+{
+    [Name("imdb_id")]
+    public required string ImdbId { get; set; }
+    [Name("title")]
+    public required string Title { get; set; }
+    [Name("year")]
+    public required int ReleaseYear { get; set; }
+    [Name("watched_at")]
+    public required DateTime WatchDate { get; set; }
+}
